7963428e7d895bbe0583fb79daefc767f70532cc
eunji <dossiba0604@gmail.com>
2025-08-20 17:55:49 +0900

    🚧 Implement toggling review-like-button

---
+++ js/review.js
@@ -0,0 +1,48 @@
+const reviewLikeButtonList = document.querySelectorAll(
+  '.review-card-footer button'
+)
+
+const HELPFUL = '도움됨'
+const NOT_HELPFUL = '도움이 돼요'
+// const checkIcon = '<i class="ic-check" aria-hidden></i>'
+
+function toggleReviewLikeButton() {
+  const isLiked = this.classList.contains('btn-primary')
+  const textElement = this.nextElementSibling
+  const reviewCardFooter = this.parentNode
+
+  if (isLiked) {
+    this.innerHTML = NOT_HELPFUL
+  } else {
+    this.innerHTML = HELPFUL
+
+    const checkIcon = document.createElement('i')
+    checkIcon.classList.add('ic-check')
+    checkIcon.setAttribute('aria-hidden', true)
+    this.prepend(checkIcon)
+  }
+
+  if (textElement) {
+    const countSpan = textElement.querySelector('span')
+    let count = Number(countSpan.innerHTML.replaceAll(',', ''))
+    count = isLiked ? count - 1 : count + 1
+
+    if (count <= 0) {
+      reviewCardFooter.removeChild(textElement)
+    } else {
+      countSpan.innerHTML = count.toLocaleString()
+    }
+  } else if (!isLiked) {
+    const newTextElement = document.createElement('p')
+    newTextElement.innerHTML =
+      '<strong><span>1</span>명</strong>에게 도움이 되었습니다.'
+    reviewCardFooter.appendChild(newTextElement)
+  }
+
+  this.classList.toggle('btn-primary')
+  this.classList.toggle('btn-outlined')
+}
+
+reviewLikeButtonList.forEach((button) => {
+  button.addEventListener('click', toggleReviewLikeButton)
+})
